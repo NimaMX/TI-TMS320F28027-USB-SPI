7ba33a1c1312532873fabffc2ee6d912be126c80
Nima Mohammadi <nima__mohammadi@outlook.com>
2021-01-08 00:15:21 +0100

    updating project structure

--- Debug/sources.mk
+++ Debug/sources.mk
@@ -112,7 +112,6 @@ PDE_DEPS__QUOTED :=
 # Every subdirectory with source files must be described here
 SUBDIRS := \
 . \
-src/app \
 src/bsp \
 src \
 
--- Debug/src/bsp/subdir_vars.mk
+++ Debug/src/bsp/subdir_vars.mk
@@ -5,50 +5,44 @@
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
 ../src/bsp/drv_base.c \
-../src/bsp/drv_cpu.c \
 ../src/bsp/drv_gpio.c \
 ../src/bsp/drv_led.c \
-../src/bsp/drv_serial.c \
+../src/bsp/drv_spi.c \
 ../src/bsp/drv_usb.c 
 
 C_DEPS += \
 ./src/bsp/drv_base.d \
-./src/bsp/drv_cpu.d \
 ./src/bsp/drv_gpio.d \
 ./src/bsp/drv_led.d \
-./src/bsp/drv_serial.d \
+./src/bsp/drv_spi.d \
 ./src/bsp/drv_usb.d 
 
 OBJS += \
 ./src/bsp/drv_base.obj \
-./src/bsp/drv_cpu.obj \
 ./src/bsp/drv_gpio.obj \
 ./src/bsp/drv_led.obj \
-./src/bsp/drv_serial.obj \
+./src/bsp/drv_spi.obj \
 ./src/bsp/drv_usb.obj 
 
 OBJS__QUOTED += \
 "src/bsp/drv_base.obj" \
-"src/bsp/drv_cpu.obj" \
 "src/bsp/drv_gpio.obj" \
 "src/bsp/drv_led.obj" \
-"src/bsp/drv_serial.obj" \
+"src/bsp/drv_spi.obj" \
 "src/bsp/drv_usb.obj" 
 
 C_DEPS__QUOTED += \
 "src/bsp/drv_base.d" \
-"src/bsp/drv_cpu.d" \
 "src/bsp/drv_gpio.d" \
 "src/bsp/drv_led.d" \
-"src/bsp/drv_serial.d" \
+"src/bsp/drv_spi.d" \
 "src/bsp/drv_usb.d" 
 
 C_SRCS__QUOTED += \
 "../src/bsp/drv_base.c" \
-"../src/bsp/drv_cpu.c" \
 "../src/bsp/drv_gpio.c" \
 "../src/bsp/drv_led.c" \
-"../src/bsp/drv_serial.c" \
+"../src/bsp/drv_spi.c" \
 "../src/bsp/drv_usb.c" 
 
 
